d7bdf0ae34dda65da57cd7671f9f6c6e07750b79
Jaiswal <2300362@cognizant.com>
2024-04-02 14:02:08 +0530

    adding Get call

--- my-app-web/src/main/java/com/jlj/myapp/PersonController.java
+++ my-app-web/src/main/java/com/jlj/myapp/PersonController.java
@@ -3,6 +3,9 @@ package com.jlj.myapp;
 import com.jlj.myapp.model.dto.PersonDTO;
 import com.jlj.myapp.model.entity.Person;
 import com.jlj.myapp.services.PersonServices;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,20 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
 
-    @Autowired
-    PersonServices personServices;
+	@Autowired
+	PersonServices personServices;
 
-    @GetMapping
-    public PersonDTO getPerson() {
-       // person.setName("Person Name 001");
-       // person.setAge(11);
-       // return person;
-        return null;
+	@GetMapping
+	public List<PersonDTO> getAllDoctors() {
 
-    }
+		return personServices.getAllPersons();
 
-    @PostMapping
-    public Person createPerson(@RequestBody PersonDTO personDTO){
-        return personServices.addPerson(personDTO);
-    }
+	}
+
+	@PostMapping
+	public Person createPerson(@RequestBody PersonDTO personDTO) {
+		return personServices.addPerson(personDTO);
+	}
 }
