66af866f4281c8c913be47003f4d159192f460cb
jaiswal88845! <jaiswal.mail2@gmail.com>
2024-04-28 16:01:09 +0530

    updating environment variables

--- my-app-frontend/src/services/DoctorService.tsx
+++ my-app-frontend/src/services/DoctorService.tsx
@@ -1,30 +1,28 @@
 import axios from "axios";
 import { Doctor } from "../interfaces/Doctor";
 
-const REST_GET_ALL_DOCTORS_URL: string = "http://localhost:8080/my-app/getAll";
-const REST_CREATE_DOCTOR_URL: string = "http://localhost:8080/my-app/doctor";
+const REST_GET_ALL_DOCTORS_URL: string =
+  import.meta.env.VITE_BACKEND_HOST_URL + "/getAll";
 
+const REST_CREATE_DOCTOR_URL: string =
+  import.meta.env.VITE_BACKEND_HOST_URL + "/doctor";
 
 export const listDoctors = () => {
   return axios.get(REST_GET_ALL_DOCTORS_URL);
 };
 
-export const createDoctor = (doctor:Doctor) => {
+export const createDoctor = (doctor: Doctor) => {
   return axios.post(REST_CREATE_DOCTOR_URL, doctor);
 };
 
-
-export const getDoctorById = (id:string) => {
-  return axios.get(REST_CREATE_DOCTOR_URL+'/'+id);
+export const getDoctorById = (id: string) => {
+  return axios.get(REST_CREATE_DOCTOR_URL + "/" + id);
 };
 
-
-export const updateDoctorById = (id:string, doctor:Doctor) => {
-  return axios.put(REST_CREATE_DOCTOR_URL+'/'+id, doctor);
+export const updateDoctorById = (id: string, doctor: Doctor) => {
+  return axios.put(REST_CREATE_DOCTOR_URL + "/" + id, doctor);
 };
 
-
-export const deleteDoctorById = (id:string | undefined) => {
-  return axios.delete(REST_CREATE_DOCTOR_URL+'/'+id);
+export const deleteDoctorById = (id: string | undefined) => {
+  return axios.delete(REST_CREATE_DOCTOR_URL + "/" + id);
 };
-
